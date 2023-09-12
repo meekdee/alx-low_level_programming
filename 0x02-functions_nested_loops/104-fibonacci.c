930fbeb65e2272dbfe24c0237aa22f0af18a6efb
meekdee <drewaju.16@gmail.com>
2023-09-12 16:00:01 -0700

    Done 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,21 +6,22 @@
  *
  * Return: Always 0 (Success).
  */
+
 int main(void)
 {
-    unsigned long num1 = 1, num2 = 2, i = 2, sum;
+	unsigned long num1 = 1, num2 = 2, i = 2, sum;
 
-    printf("%lu, %lu", num1, num2);
-    while (i < 98)
-    {
-        sum = num1 + num2;
-        printf(", %lu", sum);
-        num1 = num2;
-        num2 = sum;
-        i++;
-    }
+	printf("%lu, %lu", num1, num2);
+	while (i < 98)
+	{
+		sum = num1 + num2;
+		printf(", %lu", sum);
+		num1 = num2;
+		num2 = sum;
+		i++;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
