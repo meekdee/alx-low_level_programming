98356753b3e44a3e13b2304c70885d86ba9417e5
meekdee <drewaju.16@gmail.com>
2023-09-14 12:43:30 -0700

    Done 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the digits
+* from 0 to 9.
+*/
+
+void print_numbers(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 9)
+	{
+		_putchar(num + '0');
+		num++;
+	}
+
+	_putchar('\n');
+}
+
