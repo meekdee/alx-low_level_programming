ba8c90507c9d9968470b2c9d33afbf424e92b06f
meekdee <drewaju.16@gmail.com>
2023-09-18 14:36:18 -0700

    Done 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,7 +7,7 @@
  */
 void puts_half(char *str)
 {
-	int len, i;
+	int len, n, i;
 
 	len = 0;
 
@@ -16,13 +16,18 @@ void puts_half(char *str)
 		len++;
 	}
 
-	for (i = 0; i < len; i++)
+	if (len % 2 == 0)
 	{
-		if (i >= 5)
+		for (i = len / 2; str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
 	}
-
 	_putchar('\n');
 }
